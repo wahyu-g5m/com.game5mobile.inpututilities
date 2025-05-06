7fa050c378a5cc8e8567a77da35f6c0ad7464eac
wahyugame5mobile <wahyu@game5mobile.com>
2025-05-06 11:47:34 +0700

    chore: remove gitignore, add namespace

--- Runtime/InputUtils.cs
+++ Runtime/InputUtils.cs
@@ -2,94 +2,97 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public static class InputUtils
+namespace Five.Utils
 {
-    private static List<RaycastResult> raycastResult = new();
-
-    public static bool IsPointerOverLayer(int layerIndex)
+    public static class InputUtils
     {
-        return IsRaycastOverLayer(GetEventSystemRaycastResults(), layerIndex);
-    }
+        private static List<RaycastResult> raycastResult = new();
 
-    public static bool IsPointerOverLayer(int touchIndex, int layerIndex)
-    {
-        return IsRaycastOverLayer(GetEventSystemRaycastResults(touchIndex), layerIndex);
-    }
-
-    public static bool IsPointerOverLayer(string layerName)
-    {
-        var layerIndex = LayerMask.NameToLayer(layerName);
-        return IsPointerOverLayer(layerIndex);
-    }
-
-    public static bool IsPointerOverLayer(int touchIndex, string layerName)
-    {
-        var layerIndex = LayerMask.NameToLayer(layerName);
-        return IsRaycastOverLayer(GetEventSystemRaycastResults(touchIndex), layerIndex);
-    }
-
-    public static bool IsCustomPointerOverLayer(int layerIndex, Vector3 customPointerPosition)
-    {
-        return IsRaycastOverLayer(GetCustomEventSystemRaycastResults(customPointerPosition), layerIndex);
-    }
-
-    public static bool IsCustomPointerOverLayer(string layerName, Vector3 customPointerPosition)
-    {
-        var layerIndex = LayerMask.NameToLayer(layerName);
-        return IsCustomPointerOverLayer(layerIndex, customPointerPosition);
-    }
-
-    private static List<RaycastResult> GetEventSystemRaycastResults()
-    {
-        raycastResult.Clear();
-        var eventData = new PointerEventData(EventSystem.current);
-        eventData.position = Input.mousePosition;
-        EventSystem.current.RaycastAll(eventData, raycastResult);
-        return raycastResult;
-    }
-
-    private static List<RaycastResult> GetEventSystemRaycastResults(int touchIndex)
-    {
-        raycastResult.Clear();
-        var eventData = new PointerEventData(EventSystem.current);
-        eventData.position = Input.touches[touchIndex].position;
-        EventSystem.current.RaycastAll(eventData, raycastResult);
-        return raycastResult;
-    }
-
-    private static List<RaycastResult> GetCustomEventSystemRaycastResults(Vector3 customPointerPosition)
-    {
-        raycastResult.Clear();
-        var eventData = new PointerEventData(EventSystem.current);
-        eventData.position = customPointerPosition;
-        EventSystem.current.RaycastAll(eventData, raycastResult);
-        return raycastResult;
-    }
-
-    private static bool IsRaycastOverLayer(List<RaycastResult> eventSystemRaycastResults, int layerIndex)
-    {
-        for (int index = 0; index < eventSystemRaycastResults.Count; index++)
+        public static bool IsPointerOverLayer(int layerIndex)
         {
-            var curRaysastResult = eventSystemRaycastResults[index];
-            if (curRaysastResult.gameObject.layer == layerIndex)
-                return true;
+            return IsRaycastOverLayer(GetEventSystemRaycastResults(), layerIndex);
         }
-        return false;
-    }
 
-    public static bool IsPointerOverTag(string tag)
-    {
-        return IsRaycastOverTag(GetEventSystemRaycastResults(), tag);
-    }
-
-    private static bool IsRaycastOverTag(List<RaycastResult> eventSystemRaycastResults, string tag)
-    {
-        for (int index = 0; index < eventSystemRaycastResults.Count; index++)
+        public static bool IsPointerOverLayer(int touchIndex, int layerIndex)
         {
-            var curRaysastResult = eventSystemRaycastResults[index];
-            if (curRaysastResult.gameObject.CompareTag(tag))
-                return true;
+            return IsRaycastOverLayer(GetEventSystemRaycastResults(touchIndex), layerIndex);
         }
-        return false;
+
+        public static bool IsPointerOverLayer(string layerName)
+        {
+            var layerIndex = LayerMask.NameToLayer(layerName);
+            return IsPointerOverLayer(layerIndex);
+        }
+
+        public static bool IsPointerOverLayer(int touchIndex, string layerName)
+        {
+            var layerIndex = LayerMask.NameToLayer(layerName);
+            return IsRaycastOverLayer(GetEventSystemRaycastResults(touchIndex), layerIndex);
+        }
+
+        public static bool IsCustomPointerOverLayer(int layerIndex, Vector3 customPointerPosition)
+        {
+            return IsRaycastOverLayer(GetCustomEventSystemRaycastResults(customPointerPosition), layerIndex);
+        }
+
+        public static bool IsCustomPointerOverLayer(string layerName, Vector3 customPointerPosition)
+        {
+            var layerIndex = LayerMask.NameToLayer(layerName);
+            return IsCustomPointerOverLayer(layerIndex, customPointerPosition);
+        }
+
+        private static List<RaycastResult> GetEventSystemRaycastResults()
+        {
+            raycastResult.Clear();
+            var eventData = new PointerEventData(EventSystem.current);
+            eventData.position = Input.mousePosition;
+            EventSystem.current.RaycastAll(eventData, raycastResult);
+            return raycastResult;
+        }
+
+        private static List<RaycastResult> GetEventSystemRaycastResults(int touchIndex)
+        {
+            raycastResult.Clear();
+            var eventData = new PointerEventData(EventSystem.current);
+            eventData.position = Input.touches[touchIndex].position;
+            EventSystem.current.RaycastAll(eventData, raycastResult);
+            return raycastResult;
+        }
+
+        private static List<RaycastResult> GetCustomEventSystemRaycastResults(Vector3 customPointerPosition)
+        {
+            raycastResult.Clear();
+            var eventData = new PointerEventData(EventSystem.current);
+            eventData.position = customPointerPosition;
+            EventSystem.current.RaycastAll(eventData, raycastResult);
+            return raycastResult;
+        }
+
+        private static bool IsRaycastOverLayer(List<RaycastResult> eventSystemRaycastResults, int layerIndex)
+        {
+            for (int index = 0; index < eventSystemRaycastResults.Count; index++)
+            {
+                var curRaysastResult = eventSystemRaycastResults[index];
+                if (curRaysastResult.gameObject.layer == layerIndex)
+                    return true;
+            }
+            return false;
+        }
+
+        public static bool IsPointerOverTag(string tag)
+        {
+            return IsRaycastOverTag(GetEventSystemRaycastResults(), tag);
+        }
+
+        private static bool IsRaycastOverTag(List<RaycastResult> eventSystemRaycastResults, string tag)
+        {
+            for (int index = 0; index < eventSystemRaycastResults.Count; index++)
+            {
+                var curRaysastResult = eventSystemRaycastResults[index];
+                if (curRaysastResult.gameObject.CompareTag(tag))
+                    return true;
+            }
+            return false;
+        }
     }
 }
